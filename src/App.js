b25f104816e2b11dcee58fc16dfe693f4290b5b8
Issaka Faisal <faisal@mpharma.com>
2019-08-28 14:52:40 +0000

    added delete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,153 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Header = ({title}) => <h2>{title}</h2>
+const ContactForm = (props) => {
+  const {
+    addNew,
+    handleNameChange,
+    handlePhoneNumberChange,
+    name,
+    number
+  } = props
+
+  return(
+    <form onSubmit={addNew}>
+      <Label text={'Name:'} />
+      <Input handleChange={handleNameChange} value={name} />
+      <br />
+      <Label text={'Number:'} />
+      <Input handleChange={handlePhoneNumberChange} value={number} />
+      <br />
+      <Button type={'submit'} text={'Add'} />
+    </form>
+  )
+}
+
+const Button = ({text, type, handleClick, id}) => <button id={id} onClick={handleClick} type={type}>{text}</button>
+ 
+const Input = ({handleChange, value}) => <input onChange={handleChange} value={value} />
+
+const Label = ({text}) => <label>{text}</label>
+
+const Contact = ({contact, handleDeleteClick}) => {
+
+  return(
+    <tr>
+      <td>{contact.name}</td>
+      <td>{contact.number}</td>
+      <td><Button text={'X'} handleClick={() => handleDeleteClick(contact.id)} /></td>
+    </tr>
+  )
+}
+
+const Contacts = ({contacts, filter, setContacts}) => {
+
+  const isEmpty = (str) => str.length === 0
+
+  const contactsToShow = isEmpty(filter) ? contacts : contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
+
+  const contactList = () => contactsToShow.map(contact => { 
+    const handleDeleteClick = (contact_id) => {
+      const newContacts = contacts.filter(contact => contact.id !== contact_id)
+      setContacts(newContacts)
+    }
+    return(<Contact handleDeleteClick={()=>handleDeleteClick(contact.id)} key={contact.id} contact={contact} />)
+  })
+
+  return(
+    <table>
+      <tbody>
+        {contactList()}
+      </tbody>
+    </table>
+  )
+}
+
+const SearchForm = ({handleFilterChange, filter}) => {
+  return(
+    <form>
+      <Label text={'Filter:'} />
+      <Input handleChange={handleFilterChange} value={filter} />
+    </form>
+  )
+}
+
+const App = (props) => {
+
+  const [name, setName] = useState('a new name')
+  const [number, setNumber] = useState('')
+  const [contacts, setContacts] = useState(props.contacts)
+  const [filter, setFilter] = useState('')
+
+  const addNew = (event) => {
+    event.preventDefault()
+    const newContact = {
+      id: contacts.length + 1,
+      name: name,
+      number: number
+    }
+
+    if(isTextEmpty(name)){
+      return alert('Name cannot be blank')
+    }
+
+    if(isTextEmpty(number)){
+      return alert('Number cannot be blank')
+    }
+
+    if(isArrayEmpty(contacts)){
+      return alert(`${name} already exist`)
+    }
+    setContacts(contacts.concat(newContact))
+    setName('')
+    setNumber('')
+  }
+
+  const isArrayEmpty = (array) => {
+    let bool = false
+    if(array.filter(object => object.name === name ).length > 0){
+      bool = true
+    }
+    return bool
+  }
+
+  const isTextEmpty = (text) => {
+    let bool = false
+    if(text === ""){
+      bool = true
+    }
+    return bool
+  }
+
+  const handleNameChange = (event) => {
+    setName(event.target.value)
+  }
+
+  const handlePhoneNumberChange = (event) => {
+    setNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => setFilter(event.target.value)
+
+  const contactFormProps = {
+    addNew,
+    handleNameChange,
+    handlePhoneNumberChange,
+    name,
+    number
+  }
+
+  return(
+    <div>
+      <Header title={'Phonebook'} />
+      <ContactForm {...contactFormProps}  />
+      <br></br>
+      {contacts.length === 0 ? '' : <Header title={'Numbers'} />}
+      <SearchForm handleFilterChange={handleFilterChange} value={filter}  />
+      <br />
+      <Contacts setContacts={setContacts} contacts={contacts} filter={filter} />
     </div>
-  );
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,30 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const contacts = [
+    {
+        id: 1,
+        name: "Issaka Faisal",
+        number: "0244000000"
+    },
+    {
+        id: 2,
+        name: "Issaka Aziz",
+        number: "0244000001"
+    },
+    {
+        id: 3,
+        name: "Unaissa Faisal",
+        number: "0244000002"
+    },
+    {
+        id: 4,
+        name: "Ahmed Faisal",
+        number: "0244000003"
+    }
+]
+
+ReactDOM.render(<App contacts={contacts} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
